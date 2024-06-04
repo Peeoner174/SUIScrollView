1243a9fdee45e478e84124a9755deee2aedcc155
Pavel Kochenda <peeoner174@gmail.com>
2024-06-04 10:56:11 +0300

    Initial Commit

---
+++ SUIScrollView/SUIScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIScrollViewApp.swift
+//  SUIScrollView
+//
+//  Created by Pavel Kochenda on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SUIScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
